e183ef68cb28d6a2e1693d46e1a1bedcc0702428
Jonas Barkå <jonas.barka@gmail.com>
2024-08-09 16:57:10 +0200

    Add rudimentary measurements endpoint to functions app

---
+++ src/Azure Functions/ParticleMonitor/Measurements.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using FromBodyAttribute = Microsoft.Azure.Functions.Worker.Http.FromBodyAttribute;
+
+namespace ParticleMonitorFunctions;
+
+public class Measurements(ILogger<Measurements> logger)
+{
+    [Function("measurements")]
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequest req,
+        [FromBody] MeasurementRequest measurementRequest)
+    {
+        logger.LogInformation("Measurement recieved: {measurementRequest}", measurementRequest.ToString());
+
+        var measurementResponse = new MeasurementResponse(
+            Guid.NewGuid(),
+            DateTime.UtcNow,
+            measurementRequest.DeviceId,
+            measurementRequest.Pm10,
+            measurementRequest.Pm25,
+            measurementRequest.Pm100
+        );
+
+        return new OkObjectResult(measurementResponse);
+    }
+}
+
+public record MeasurementRequest(int DeviceId, int Pm10, int Pm25, int Pm100);
+
+public record MeasurementResponse(Guid Guid, DateTime DateTime, int DeviceId, int Pm10, int Pm25, int Pm100);
