df32046d06cbeb55f1e98240f10a21dc9caa1e6d
Jonas Barkå <jonas.barka@gmail.com>
2024-08-09 13:28:52 +0200

    Add post to server after 12 reads (one minute)

--- src/microcontroller/code.py
+++ src/microcontroller/code.py
@@ -75,31 +75,37 @@ def read_sensor_data(pm25):
             time.sleep(5)
 
 def loop(connection, pm25, online_mode):
+    READ_CADENCE = 5
+    READS_BEFORE_POST = 12
+    RETRIES_BEFORE_OFFLINE = 3
+    reads_since_last_post = 0
     retries = 0
     while True:
         sensor_data = read_sensor_data(pm25)
 
-        if online_mode:
+        if online_mode and reads_since_last_post == READS_BEFORE_POST :
+            reads_since_last_post = 0
             try:
                 server.post_measurement(connection, sensor_data)
                 retries = 0
 
             except Exception as err:
                 print(f"❌ Error posting measurements to server: {err}")
-                if retries >= 5:
+                if retries >= RETRIES_BEFORE_OFFLINE:
                     print("Switching to offline mode.")
                     online_mode = False
                     retries = 0
-                    time.sleep(5)
+                    time.sleep(READ_CADENCE)
                     continue
 
                 print("Retrying...")
                 display.failed_and_retrying("Posting to server")
-                time.sleep(5)
+                time.sleep(READ_CADENCE)
                 retries += 1
                 continue
 
+        reads_since_last_post += 1
         display.print_sensor_data(sensor_data, online_mode)
-        time.sleep(5)
+        time.sleep(READ_CADENCE)
 
 main()
